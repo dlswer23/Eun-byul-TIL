c2ec11101baf00e0c53bcfa3b86a024d0e6d88c7
이은별 <lbj4298@naver.com>
2020-10-21 15:18:10 +0900

    [ADD] 큐 삭제

---
+++ 자료구조/큐 삭제.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int rear = -1;
+int front = -1;
+int addq(int* ptr, int value);
+int main() {
+	int arr[10];
+	for (int i = 0; i < 10; i++) addq(arr, i);
+	for (int i = 0; i < 10; i++) printf("%d ", arr[i]);
+	printf("\nrear: %d", rear);
+}
+int addq(int* ptr, int value) {
+	if (rear >= 9) return printf("QUEUE is FULL!");
+	else ptr[++rear] = value;
+}
+int deleteq(int* ptr) {
+	if (rear==front) return printf("QUEUE is EMPTY!");
+	else return ptr[++front];
+
+	
+}
