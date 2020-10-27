e92921bcab373ddc68bf0ae68c90dba313bc3d2e
이은별 <lbj4298@naver.com>
2020-10-27 11:16:12 +0900

    [MODIFY] CMyData 수정

--- 자료구조/CMyData.cpp
+++ 자료구조/CMyData.cpp
@@ -14,7 +14,7 @@ public:
 	int GetData() const {
 		return m_nX;}
 	void SetData(int nX) {
-		m_nX = nX;
+		m_nX = nX; //내가 원하는 값으로 저장하기 위하여 == 사용자의 입력 값으로 직접 저장 
 	}
 
 private:
@@ -24,32 +24,44 @@ private:
 
 
 
-	class CMyDataEx : public CMyData {
-		CMyDataEx() {
-			CMyData::SetData(nX);
+	class CMyDataEx : public CMyData
+	{
+	public:
 
-			m_nY = nY;
-		}
-		int GetX();
+		CMyDataEx() {}
+		CMyDataEx(int nX,int nY) {}
+
+		int GetX()
 		{
 			return CMyData::GetData();
 		}
-		int  GetY();
+		int  GetY()
 		{
 			return m_nY;
 		}
 
+		void SetData(int nX, int nY) {
+			CMyData::SetData(nX);
 
 
-		CMyDataEx(int nX, int nY) {
-			
 		}
 
 	private:
 		int m_nY;
+
 	};
 
 	int main() {
 		CMyDataEx test;
+		test.SetData(10, 20);
+		cout << test.GetData() << endl;
+		cout << test.GetData() << endl;
 
 	}
+
+	//CMyData클래스
+	//멤버변수 int 형 m_nX
+	//멤버함수 
+	//1. 디폴트 생성자 함수 
+	//2.매개변수 int nV를 가지는 생성자 함수
+	//3. 멤버변수 m_nX값을 반환하는 GetData()함수
