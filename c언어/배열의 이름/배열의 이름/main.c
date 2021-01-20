afb7edd0e71f438cd62e4298adacfc7f34f54de7
dlswer23 <lbj4298@naver.com>
2021-01-20 17:38:42 +0900

    [ADD] 배열이름은 배열의 시작주소

--- c언어/배열의 이름/배열의 이름/main.c
+++ c언어/배열의 이름/배열의 이름/main.c
@@ -7,8 +7,24 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+int main(void){
+    
+    int arrayNumber[3]={10,20,30};
+    *arrayNumber = 50;
+    
+    printf("배열의 이름 : %p \n", arrayNumber);
+    printf("첫번째 원소의 주소 값 : %p \n", &arrayNumber[0]);
+    printf("두번째 원소의 주소 값 : %p \n", &arrayNumber[1]);
+    printf("세번째 원소의 주소 값 : %p \n", &arrayNumber[2]);
+    
+    printf("%d \n", *arrayNumber);
+    
     return 0;
+    
+    /*결과값은
+    첫번째 원소의 주소 값 : 0x7ffeefbff37c
+    두번째 원소의 주소 값 : 0x7ffeefbff380
+    세번째 원소의 주소 값 : 0x7ffeefbff384*/
+    
 }
