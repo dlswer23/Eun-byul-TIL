030f05aa6edf236a3b6868cd99b134b9cd9cd9a3
dlswer23 <lbj4298@naver.com>
2021-01-20 17:24:06 +0900

    [ADD] 배열의 이름은 배열의 시작주소.use.XCODE

---
+++ c언어/배열의 이름/배열의 이름/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  배열의 이름
+//
+//  Created by 이은별 on 2021/01/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
