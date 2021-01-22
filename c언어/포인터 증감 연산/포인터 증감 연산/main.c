b84dae322ea97570895f5bdf03b149bf03913356
dlswer23 <lbj4298@naver.com>
2021-01-22 15:50:32 +0900

    [ADD EXAM] 포인터 증감 연산 예시 추가

--- c언어/포인터 증감 연산/포인터 증감 연산/main.c
+++ c언어/포인터 증감 연산/포인터 증감 연산/main.c
@@ -24,5 +24,35 @@ int main(void){
     printf("세번째 원소의 값: %d \n",*(p1+2));
     
     
+    
+    
+    printf("--------------------\n");
+    
+    int an[3] ={10,20,30};
+    
+    
+    printf("1 : %d\n", an[0]);
+    printf("2 : %d\n", an[1]);
+    printf("3 : %d\n", an[2]);
+    
+    printf("\n");
+    
+    
+    
+    int* ptran = an;
+    
+    printf("1: %d\n",*ptran);
+    printf("2: %d\n",*(ptran+3));
+    printf("3: %d\n",*(ptran+2));
+    
+
+    char* str1 = NULL;
+    str1 = "suhyun"; //문자열 값을 한번에 초기화 시켜줄 수 있다.
+    
+    char str2[100]="";
+    str2[0] = 's'; //scanf를 통해서 정상적으로 값을 입력 받을 수 있다.
+    
+    
+    
    
 }
